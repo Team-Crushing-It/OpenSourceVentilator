d7742a0e963b5cb487262dd5cefc8a1982e68441
Gene-Dana <yllanesgene@gmail.com>
2020-04-07 11:00:22 -0400

    removed app

--- in-vent/lib/models/list_item.dart
+++
@@ -1,6 +0,0 @@
-class ListItem {
-  final String title;
-  final String description;
-
-  ListItem({this.title, this.description});
-}
--- in-vent/lib/ui/shared/font_styles.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-import './app_colors.dart';
-
-const TextStyle viewTitle = TextStyle(fontSize: 20.0, fontWeight: FontWeight.w800);
-const TextStyle viewErrorTitle = TextStyle(fontSize: 20.0, fontWeight: FontWeight.w800, color: lightGrey);
